422966f31d5aa38628efcab373c5820078431814
Mariam <mero23862@gmail.com>
2025-02-04 23:55:00 +0200

    Theming Done

---
+++ lib/My_Theme/light_theme.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/material/theme_data.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movie_app/My_Theme/theme.dart';
+
+class LightTheme extends BaseLine {
+  @override
+  Color get scaffoldBackgroundColor => Color(0xFFFFFFFF);
+
+  @override
+  Color get primaryColor => Color(0xFFF6BD00);
+
+  @override
+  Color get focusColor => Color(0xFF282A28);
+
+  @override
+  Color get textColor => Color(0xFF121312);
+
+  @override
+  ThemeData get themeData => ThemeData(
+        scaffoldBackgroundColor: scaffoldBackgroundColor,
+        primaryColor: primaryColor,
+        focusColor: focusColor,
+        textTheme: TextTheme(
+          titleSmall: GoogleFonts.roboto(
+              fontSize: 16, fontWeight: FontWeight.w700, color: textColor),
+          titleMedium: GoogleFonts.roboto(
+              fontSize: 20, fontWeight: FontWeight.w700, color: textColor),
+          titleLarge: GoogleFonts.roboto(
+              fontSize: 24, fontWeight: FontWeight.w700, color: textColor),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: focusColor,
+          selectedItemColor: primaryColor,
+          unselectedItemColor: textColor,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          type: BottomNavigationBarType.fixed,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(primaryColor),
+          ),
+        ),
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/My_Theme/dark_theme.dart';
+import 'package:movie_app/My_Theme/theme.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MovieApp());
+import 'My_Provider/my_provider.dart';
+import 'My_Theme/light_theme.dart';
+
+void main() async {
+  runApp(
+    await ChangeNotifierProvider(
+      create: (context) => MyProvider(),
+      child: MovieApp(),
+    ),
+  );
 }
 
 class MovieApp extends StatelessWidget {
@@ -10,8 +21,14 @@ class MovieApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<MyProvider>(context);
+    BaseLine lightTheme = LightTheme();
+    BaseLine darkTheme = DarkTheme();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: lightTheme.themeData,
+      darkTheme: darkTheme.themeData,
+      themeMode: provider.themeMode,
     );
   }
 }
