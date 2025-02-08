024b58a5af7ab36cd34f8c79b96657652610ebc1
Mariam <mero23862@gmail.com>
2025-02-08 19:42:20 +0200

    splash screen DONE

--- lib/screens/Login_Screens/register_screen.dart
+++ lib/screens/Login_Screens/register_screen.dart
@@ -15,9 +15,7 @@ import 'login_screen.dart';
 class RegisterScreen extends StatefulWidget {
   static const String routeName = "Register Screen";
 
-  final String title;
-
-  RegisterScreen({super.key, required this.title});
+  RegisterScreen({super.key});
 
   @override
   State<RegisterScreen> createState() => _RegisterScreenState();
